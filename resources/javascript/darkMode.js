61d7ae7d55a3be33d6795f238774a5402bc2c5a0
Your Name <Victor.Andrade@rdm.com>
2022-09-16 08:50:26 -0300

    auterações nas tabelas, css e footer

--- resources/javascript/darkMode.js
+++ resources/javascript/darkMode.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeStorage = localStorage.getItem('dark-mode')
-    const html = document.querySelector('body')
+    const body = document.querySelector('body')
     const inputDarkMode = document.getElementById('input-dark-mode')
 
     if(darkModeStorage){
-        html.setAttribute("dark", "true")
+        body.setAttribute("dark", "true")
     }
 
     inputDarkMode.addEventListener('change', () => {
         if(inputDarkMode.checked){
-            html.setAttribute("dark", "true")
+            body.setAttribute("dark", "true")
             localStorage.setItem('dark-mode', true)
         }else{
-            html.removeAttribute("dark")
+            body.removeAttribute("dark")
             localStorage.removeItem('dark-mode')
         }
    })
